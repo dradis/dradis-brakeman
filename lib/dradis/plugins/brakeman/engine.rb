a426087fdf163a4058d42d84a29c95096ce518af
Daniel Martin <etd-github@nomejortu.com>
2015-08-17 10:18:16 +0200

    Type s/Nmap/Brakeman/

--- lib/dradis/plugins/brakeman/engine.rb
+++ lib/dradis/plugins/brakeman/engine.rb
@@ -2,7 +2,7 @@ module Dradis
   module Plugins
     module Brakeman
       class Engine < ::Rails::Engine
-        isolate_namespace Dradis::Plugins::Nmap
+        isolate_namespace Dradis::Plugins::Brakeman
 
         include ::Dradis::Plugins::Base
         description 'Processes Brakeman JSON output, use: brakeman -f json -o results.json'
