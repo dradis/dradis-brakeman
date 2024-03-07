f3bdf12f2f8097cb0eb9de43509003a79f841ce1
Matt Budz <matt.budz@protonmail.com>
2024-03-07 17:04:49 +0100

    add default mapping

--- lib/dradis/plugins/brakeman/mapping.rb
+++ lib/dradis/plugins/brakeman/mapping.rb
@@ -1,4 +1,4 @@
-module Dradis::Plugins::Qualys
+module Dradis::Plugins::Brakeman
   module Mapping
 
     def self.component_name
@@ -6,6 +6,23 @@ module Dradis::Plugins::Qualys
     end
 
     def self.default_mapping
+      {
+        'scan-info' => {
+          'Title' => 'Brakeman scan information',
+          'Application' => '{{ brakeman[scan_info.app_path] }}',
+          'BrakemanVersion' => '{{ brakeman[scan_info.brakeman_version] }}',
+          'RailsVersion' => '{{ brakeman[scan_info.rails_version] }}',
+          'WarningCount' => '{{ brakeman[scan_info.security_warnings] }}'
+        },
+        'warning' => {
+          'Title' => '{{ brakeman[warning.message] }}',
+          'Type' => '{{ brakeman[warning.warning_type] }}',
+          'Confidence' => '{{ brakeman[warning.confidence] }}',
+          'Path' => '{{ brakeman[warning.file] }}#{{ brakeman[warning.line] }}',
+          'Code' => 'bc.. {{ brakeman[warning.code] }}',
+          'References' => '{{ brakeman[warning.link] }}'
+        }      
+      }
     end
   end
 end
