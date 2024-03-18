b1d87fd290529e0428d44595f92ed17ef80a3608
Matt Budz <matt.budz@protonmail.com>
2024-03-18 19:13:17 +0100

    use constant for default mapping

--- lib/dradis/plugins/brakeman/mapping.rb
+++ lib/dradis/plugins/brakeman/mapping.rb
@@ -1,28 +1,21 @@
 module Dradis::Plugins::Brakeman
   module Mapping
-
-    def self.component_name
-      'brakeman'
-    end
-
-    def self.default_mapping
-      {
-        'scan_info' => {
-          'Title' => 'Brakeman scan information',
-          'Application' => '{{ brakeman[scan_info.app_path] }}',
-          'BrakemanVersion' => '{{ brakeman[scan_info.brakeman_version] }}',
-          'RailsVersion' => '{{ brakeman[scan_info.rails_version] }}',
-          'WarningCount' => '{{ brakeman[scan_info.security_warnings] }}'
-        },
-        'warning' => {
-          'Title' => '{{ brakeman[warning.message] }}',
-          'Type' => '{{ brakeman[warning.warning_type] }}',
-          'Confidence' => '{{ brakeman[warning.confidence] }}',
-          'Path' => '{{ brakeman[warning.file] }}#{{ brakeman[warning.line] }}',
-          'Code' => 'bc.. {{ brakeman[warning.code] }}',
-          'References' => '{{ brakeman[warning.link] }}'
-        }      
-      }
-    end
+    DEFAULT_MAPPING = {
+      scan_info: {
+        'Title' => 'Brakeman scan information',
+        'Application' => '{{ brakeman[scan_info.app_path] }}',
+        'BrakemanVersion' => '{{ brakeman[scan_info.brakeman_version] }}',
+        'RailsVersion' => '{{ brakeman[scan_info.rails_version] }}',
+        'WarningCount' => '{{ brakeman[scan_info.security_warnings] }}'
+      },
+      warning: {
+        'Title' => '{{ brakeman[warning.message] }}',
+        'Type' => '{{ brakeman[warning.warning_type] }}',
+        'Confidence' => '{{ brakeman[warning.confidence] }}',
+        'Path' => '{{ brakeman[warning.file] }}#{{ brakeman[warning.line] }}',
+        'Code' => 'bc.. {{ brakeman[warning.code] }}',
+        'References' => '{{ brakeman[warning.link] }}'
+      }      
+    }.freeze
   end
 end
